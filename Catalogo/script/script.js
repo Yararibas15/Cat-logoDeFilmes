13b73793b44e53487620d9f86d798b52396eba7e
Yararibas15 <yara.faustino@escola.pr.gov.br>
2026-06-01 11:24:05 -0300

    Add files via upload

---
+++ Catalogo/script/script.js
@@ -0,0 +1,32 @@
+const filmes = [
+     {
+         título: "Vingadores",
+         imagem:"https://www.themoviedb.org/movie/299534-avengers-endgame",
+         descricao: "Ultimato",
+     },
+     {
+         título: "Batman",
+         imagem:"https://www.themoviedb.org/movie/155-the-dark-knight?language=pt-BR", 
+         descrição: "Cavaleiro das trevas",
+     },
+     { 
+         título: "Diário de uma Paixão",
+         imagem:"https://www.themoviedb.org/movie/11036-the-notebook?language=pt-BR", 
+         descrição: "Romance",
+     },
+     {
+         título: "Esposa de Mentirinha",
+         imagem:"https://www.themoviedb.org/movie/50546-just-go-with-it?language=pt-BR", 
+          descrição: "Comédia",
+     },
+     {
+         título: "Gente Grande 2",
+         imagem:"https://www.themoviedb.org/movie/109418-grown-ups-2?language=pt-BR", 
+          descrição: "Comédia",
+     },
+     {
+         título: "Minha culpa",
+         imagem:"https://www.themoviedb.org/movie/1010581-culpa-mia?language=pt-BR",
+          descrição: "Drama",
+     },
+]
