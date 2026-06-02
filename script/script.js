42a2fca990ff9e5aa8102a15bf83bda045902d4b
Yararibas15 <yara.faustino@escola.pr.gov.br>
2026-06-02 10:54:10 -0300

    ok

--- script/script.js
+++ script/script.js
@@ -1,36 +1,36 @@
 const filmes = [
-     {
-         título: "Vingadores",
-         imagem:"https://www.themoviedb.org/movie/299534-avengers-endgame",
-         descricao: "Ultimato",
-     },
-     {
-         título: "Batman",
-         imagem:"https://www.themoviedb.org/movie/155-the-dark-knight?language=pt-BR", 
-         descrição: "Cavaleiro das trevas",
-     },
-     { 
-         título: "Diário de uma Paixão",
-         imagem:"https://www.themoviedb.org/movie/11036-the-notebook?language=pt-BR", 
-         descrição: "Romance",
-     },
-     {
-         título: "Esposa de Mentirinha",
-         imagem:"https://www.themoviedb.org/movie/50546-just-go-with-it?language=pt-BR", 
-          descrição: "Comédia",
-     },
-     {
-         título: "Gente Grande 2",
-         imagem:"https://www.themoviedb.org/movie/109418-grown-ups-2?language=pt-BR", 
-          descrição: "Comédia",
-     },
-     {
-         título: "Minha culpa",
-         imagem:"https://www.themoviedb.org/movie/1010581-culpa-mia?language=pt-BR",
-          descrição: "Drama",
-     },
-    ]
-   const listaFilmes = document.getElementById("listaFilmes");
+    {
+        título: "Vingadores",
+        imagem: "https://www.themoviedb.org/movie/299534-avengers-endgame",
+        descricao: "Ultimato",
+    },
+    {
+        título: "Batman",
+        imagem: "https://www.themoviedb.org/movie/155-the-dark-knight?language=pt-BR",
+        descrição: "Cavaleiro das trevas",
+    },
+    {
+        título: "Diário de uma Paixão",
+        imagem: "https://www.themoviedb.org/movie/11036-the-notebook?language=pt-BR",
+        descrição: "Romance",
+    },
+    {
+        título: "Esposa de Mentirinha",
+        imagem: "https://www.themoviedb.org/movie/50546-just-go-with-it?language=pt-BR",
+        descrição: "Comédia",
+    },
+    {
+        título: "Gente Grande 2",
+        imagem: "https://www.themoviedb.org/movie/109418-grown-ups-2?language=pt-BR",
+        descrição: "Comédia",
+    },
+    {
+        título: "Minha culpa",
+        imagem: "https://www.themoviedb.org/movie/1010581-culpa-mia?language=pt-BR",
+        descrição: "Drama",
+    },
+]
+const listaFilmes = document.getElementById("listaFilmes");
 function mostrarFilmes(lista) {
     listaFilmes.innerHTML = ""
     lista.forEach(filme => {
@@ -39,9 +39,9 @@ function mostrarFilmes(lista) {
                 <div class="card card-filme h-100">
                     <img src="${filme.imagem}" class="card-img-top">
                     <div class="card-body">
-                        <h3>${filme.titulo}</h3>
+                        <h3>${filme.título}</h3>
                         <p>${filme.descrição}</p>
-                        <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes"</button>
+                        <button class="btn btn-dark" onclick="verDetalhes('${filme.título}')">Ver detalhes"</button>
                     </div>
                 </div>
             </div>    
@@ -53,8 +53,8 @@ mostrarFilmes(filmes)
 
 function verDetalhes(titulo) {
     Swal.fire({
-        title: titulo,
-        text:"Mais informaçaões de Filme",
-        icon:"info"
+        title: título,
+        text: "Mais informaçaões de Filme",
+        icon: "info"
     })
 }
