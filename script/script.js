1d44625253e01900aedc4a5a85e08a3d2e8bc999
Yararibas15 <yara.faustino@escola.pr.gov.br>
2026-06-02 11:06:58 -0300

    ok

--- script/script.js
+++ script/script.js
@@ -1,32 +1,32 @@
 const filmes = [
     {
         título: "Vingadores",
-        imagem: "https://www.themoviedb.org/movie/299534-avengers-endgame",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg",
         descricao: "Ultimato",
     },
     {
         título: "Batman",
-        imagem: "https://www.themoviedb.org/movie/155-the-dark-knight?language=pt-BR",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4lj1ikfsSmMZNyfdi8R8Tv5tsgb.jpg",
         descrição: "Cavaleiro das trevas",
     },
     {
         título: "Diário de uma Paixão",
-        imagem: "https://www.themoviedb.org/movie/11036-the-notebook?language=pt-BR",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/hO6k34ZNDwWzgcnzFbqYf2Rjg5W.jpg",
         descrição: "Romance",
     },
     {
         título: "Esposa de Mentirinha",
-        imagem: "https://www.themoviedb.org/movie/50546-just-go-with-it?language=pt-BR",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/glC4pDnSieQ9NGU501rfVIqUCTB.jpg",
         descrição: "Comédia",
     },
     {
         título: "Gente Grande 2",
-        imagem: "https://www.themoviedb.org/movie/109418-grown-ups-2?language=pt-BR",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/fKYW0oKPvy6HF8x5egL1ljC36xb.jpg",
         descrição: "Comédia",
     },
     {
         título: "Minha culpa",
-        imagem: "https://www.themoviedb.org/movie/1010581-culpa-mia?language=pt-BR",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/a59iGvF609yAZ5tfZUoQ3XipeQH.jpg",
         descrição: "Drama",
     },
 ]
